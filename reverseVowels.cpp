350591c4d31058187a54fb95934fc93e84fe4153
ROOH-FATIMA <roohf75@gmail.com>
2024-08-30 09:53:20 -0700

    PRQ

--- reverseVowels.cpp
+++ reverseVowels.cpp
@@ -1,34 +1,32 @@
 #include <iostream>
 using namespace std;
-
-void reverseVowels(char arr[]){
-    string vowels = "aeiouAEIOU";
+void reverseVowels(char a[]) {
     int left = 0;
     int right = 0;
-    while(arr[right]!='\0'){
+
+    while (a[right]!='\0'){
         right++;
     }
     right--;
-     while (left < right) {
-        if (vowels.find(arr[left]) == arr[left]) 
-        {
+
+    while (left<right){
+        if (!(a[left]=='a'||a[left]=='e'||a[left]=='i'||a[left]=='o'||a[left] =='u'||a[left]=='A'||a[left]=='E'||a[left]=='I'||a[left]=='O'||a[left]=='U')) {
             left++;
-        }
-         else if (vowels.find(arr[right]) ==arr[right] )
-        {
+        } else if (!(a[right]=='a'||a[right]=='e'||a[right]=='i'||a[right]=='o'||a[right]=='u'||a[right]=='A'||a[right]=='E'||a[right]=='I'||a[right]=='O'||a[right]=='U')) {
+            right--;
+        } else {
+            char temp=a[left];
+            a[left]=a[right];
+            a[right]=temp;
+            left++;
             right--;
         }
-
     }
-    
 }
 
 int main() {
-    char arr[] ="hello";
-
-    reverseVowels(arr);
-
-    cout<<"reverse vowels: "<<arr<<endl;
-
+    char a[]="hello";
+    reverseVowels(a);
+    cout << "reverse vowels: "<<a<<endl;
     return 0;
 }
